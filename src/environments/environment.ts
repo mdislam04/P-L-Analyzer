d759cba23bb5a826eb627eaffde9b66c6417a97d
Islam <mohmmad.islam@accenture.com>
2025-12-04 19:41:40 +0530

    added missing environment.ts file

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  googleDrive: {
+    clientId: '415057442876-1lpa3bgneif4l2gnd6g1anml9o49hl0l.apps.googleusercontent.com',
+    redirectUri: 'http://localhost:4200',
+    scope: 'https://www.googleapis.com/auth/drive.file'
+  }
+};
